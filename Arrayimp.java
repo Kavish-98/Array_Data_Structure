631e5f696bb62a1b1b2155c1bc73e7ef5cf894f2
Kavish-98 <vimansha98@gmail.com>
2025-05-22 08:42:53 +0530

    User Input
    Got input from user and print array

--- Arrayimp.java
+++ Arrayimp.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
 public class Arrayimp {
     public static void main(String[] args){
 
-        char[] arr = {'A','B','C',};
+        int[] arr = new int[5];
+        int size = 5;
 
-        for(int i =0; i<arr.length;i++){
+        Scanner UserIn = new Scanner(System.in);
+        
+
+        for(int j=0; j<size;j++){
+            arr[j] = UserIn.nextInt();
+        }
+        System.out.println("\n");
+        for(int i =0; i<size;i++){
             System.out.println(arr[i]);
         }
     }
-    
-    
 }

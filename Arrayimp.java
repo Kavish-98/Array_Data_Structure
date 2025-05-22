31b653363fbfbd4f20674ff0a973bf9149498c54
Kavish-98 <vimansha98@gmail.com>
2025-05-22 07:32:08 +0530

    How to check array length

--- Arrayimp.java
+++ Arrayimp.java
@@ -1,9 +1,9 @@
 public class Arrayimp {
     public static void main(String[] args){
 
-        int[] arr = {10,20,30,40,50};
+        char[] arr = {'A','B','C',};
 
-        System.out.println(arr[2]);
+        System.out.println(arr.length);
     }
     
     

60870c1693a5da12687bb89eefc874d132dba801
Kavish-98 <vimansha98@gmail.com>
2025-05-22 10:25:18 +0530

    Delete index and print array

--- Arrayimp.java
+++ Arrayimp.java
@@ -18,19 +18,13 @@ public class Arrayimp {
         System.out.println("Enter the index: ");
             int ind = UserIn.nextInt();
 
-        System.out.println("Enter the Element: ");
-            int ele = UserIn.nextInt();
-
-        size++;
-
-        for(int k=size; k>=ind;k--){
-            arr[k]=arr[k-1];
+        for (int j=ind; j<size; j++){
+            arr[j] = arr[j+1];
         }
-        arr[ind]=ele;
-
-        System.out.println("Output of array: ");
+        size--;
+          System.out.println("Output of array: ");
         for(int i =0; i<size;i++){
             System.out.println(arr[i]);
-    }
+        }
  }
 }

3f5a5ff1218b83e2f97a56954e7d104f1abaeea9
Kavish-98 <vimansha98@gmail.com>
2025-05-22 09:09:17 +0530

    Input new element in array

--- Arrayimp.java
+++ Arrayimp.java
@@ -2,18 +2,35 @@ import java.util.Scanner;
 public class Arrayimp {
     public static void main(String[] args){
 
-        int[] arr = new int[5];
+        int[] arr = new int[10];
         int size = 5;
 
         Scanner UserIn = new Scanner(System.in);
         
-
+        System.out.println("Enter Data: ");
         for(int j=0; j<size;j++){
             arr[j] = UserIn.nextInt();
         }
-        System.out.println("\n");
+        System.out.println("Output of array: ");
         for(int i =0; i<size;i++){
             System.out.println(arr[i]);
         }
+        System.out.println("Enter the index: ");
+            int ind = UserIn.nextInt();
+
+        System.out.println("Enter the Element: ");
+            int ele = UserIn.nextInt();
+
+        size++;
+
+        for(int k=size; k>=ind;k--){
+            arr[k]=arr[k-1];
+        }
+        arr[ind]=ele;
+
+        System.out.println("Output of array: ");
+        for(int i =0; i<size;i++){
+            System.out.println(arr[i]);
     }
+ }
 }
